12c59871773d2787ce120e98daf25845f28f42a1
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2020-08-13 14:39:09 -0600

    About page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountsRouter = require('./routes/accounts');
 var loginRouter = require('./routes/login');
+var aboutRouter = require('./routes/about')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/home', usersRouter)
 app.use('/accounts', accountsRouter);
 app.use('/login', loginRouter)
+app.use('/team', aboutRouter )
 
 
 // catch 404 and forward to error handler
