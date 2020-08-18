21a1851ccb87d29106ba05202962ef39b3776d6b
Yaksh Patel <yaksh3636@gmail.com>
2020-08-18 14:11:08 -0600

    Login ui completed.

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.render('login-n');
+    res.render('login_screen');
 });
 
 module.exports = router;
