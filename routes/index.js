a9e44ffa8357e314e6064843ad867d1a5b42496d
Yaksh Patel <yaksh3636@gmail.com>
2020-08-18 14:16:06 -0600

    Login ui merge fix

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '360 Financial Tools' });
-  jwtHelper.timeMethod();
-
-  
-
-
+  //jwtHelper.timeMethod();
 });
 
 module.exports = router;
