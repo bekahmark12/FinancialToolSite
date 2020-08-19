4101fa19c7d3b9c9995ca3030c0b89795d84e3dd
bekahmark12 <rebekahmark12@gmail.com>
2020-08-19 11:11:04 -0600

    Api requests complete

--- routes/apiHelper.js
+++ routes/apiHelper.js
@@ -27,9 +27,10 @@ class apiHelper{
         }
 
         request(options,function (error, response, body) {
-            console.error('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            // console.error('error:', error); // Print the error if one occurred
+            // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            // console.log('body:', body); // Print the HTML for the Google homepage.
+            return body
         });
     }
 
@@ -44,9 +45,10 @@ class apiHelper{
         }
 
         request(options,function (error, response, body) {
-            console.error('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            // console.error('error:', error); // Print the error if one occurred
+            // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            // console.log('body:', body); // Print the HTML for the Google homepage.
+            return body
         });
     }
 }
