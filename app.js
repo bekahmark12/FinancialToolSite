a3c8495bd6f653d4d3b5ddf30029e127ca9ede5a
Yaksh Patel <yaksh3636@gmail.com>
2020-08-19 10:08:35 -0600

    login post method

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
+/** IMPORTS */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyparser = require('body-parser');
 
+
+var app = express();
+
+//Debugging logger
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyparser.json)
+// app.use(bodyparser.urlencoded({ extended: false }));
+
+/** ROUTES */
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountsRouter = require('./routes/accounts');
 var loginRouter = require('./routes/login');
 var aboutRouter = require('./routes/about');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+//Set the path redirection
 app.use('/', indexRouter);
 app.use('/home', usersRouter)
 app.use('/accounts', accountsRouter);
