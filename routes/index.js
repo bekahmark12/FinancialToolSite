70b749c33789246bdd4c296c2b92f8702a8bbe5f
bekahmark12 <rebekahmark12@gmail.com>
2020-08-19 11:10:06 -0600

    Api requests complete

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@
 
 var express = require('express');
 var router = express.Router();
+let apiHelper;
+apiHelper = require('./apiHelper.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '360 Financial Tools' });
-  //jwtHelper.timeMethod();
+
+  apiHelper.getAccountData()
 });
 
 module.exports = router;
