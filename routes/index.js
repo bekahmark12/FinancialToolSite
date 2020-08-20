0d6c286fdc15d858d78a8c6e1610929c16b4039d
Yaksh Patel <yaksh3636@gmail.com>
2020-08-20 12:22:08 -0600

    Login screen required attribute added.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 let apiHelper;
 apiHelper = require('./apiHelper.js');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '360 Financial Tools' });
