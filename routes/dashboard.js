23a38917c55e221860487da3d67ff95223d44b4b
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2020-08-24 16:55:22 -0600

    dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 router.get("/", function(req, res, next){
     res.render("dashboard", { displayName: req.session.displayName});
+    //res.render("dashboard");
     console.log("Getting dashboard.....")
     console.log(req.body.user)
 });
